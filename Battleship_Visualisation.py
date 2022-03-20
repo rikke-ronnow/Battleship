c51861309bba954e69c1a2a982c47aeef4762dc8
rr420-star <rr420@ic.ac.uk>
2022-03-20 16:17:17 +0100

    Changed the game so ship placement is random

--- Battleship_Visualisation.py
+++ Battleship_Visualisation.py
@@ -129,30 +129,82 @@ def less_random_guess():
         x=random.randint(0,9)
         y=random.randint(0,9)
     return np.array([x,y])
-            
+
+
+def ship_validation(board, ship):
+    x,y = ship.pos()
+    for c in ship.pos():
+        if c < 0 or c > 9:
+            return False
+    
+    if ship.size() < 1:
+        return False
+    
+    if ship.orient() == "Horizontal":
+        if x+ship.size()-1>9:
+            return False
+        for i in range(ship.size()):
+            if board[x+i][y] != 0:
+                return False
+    elif ship.orient() == "Vertical":
+        if y+ship.size()-1>9:
+            return False
+        for i in range(ship.size()):
+            if board[x][y+i] != 0:
+                return False
+    else:
+        return False
+    return True
+      
+def ship_placement_random(l:list=[2,3,3,4,5]):
+    board_temp = np.zeros((10,10))
+    ship_arr = np.array([])
+    for length in l:
+        while True:
+            ship_temp = Ship(length, 
+                             np.array([random.randint(0,9),random.randint(0,9)]), 
+                             random.choice(["Horizontal", "Vertical"]))
+            if ship_validation(board_temp, ship_temp):
+                break
+        #while not ship_validation(board_temp, ship_temp):
+        #    ship_temp = Ship(length, 
+        #                     np.array([random.randint(0,9),random.randint(0,9)]), 
+        #                     random.choice(["Horizontal", "Vertical"]))
+        x_temp, y_temp = ship_temp.pos()
+        if ship_temp.orient() == "Horizontal":
+            for i in range(length):
+                board_temp[x_temp+i][y_temp] = 1
+        elif ship_temp.orient() == "Vertical":
+            for i in range(length):
+                board_temp[x_temp][y_temp+i] = 1
+        ship_arr=np.append(ship_arr,ship_temp)
+    #print(board_temp)
+    return ship_arr
+      
 #%%
 
-ship_board=np.zeros((10,10))
-guess_board=np.zeros((10,10))
+# ship_board=np.zeros((10,10))
+# guess_board=np.zeros((10,10))
 
-ship_array=np.array([])
+# ship_array=np.array([])
 
-ship1=Ship()
+# ship1=Ship()
 
-ship_array=np.append(ship_array,ship1)
+# ship_array=np.append(ship_array,ship1)
 
 
-ship2=Ship(3,np.array([2,1]),"Vertical")
+# ship2=Ship(3,np.array([2,1]),"Vertical")
 
-ship_array=np.append(ship_array,ship2)
+# ship_array=np.append(ship_array,ship2)
 
-ship3=Ship(4, np.array([6,6]),"Horizontal")
+# ship3=Ship(4, np.array([6,6]),"Horizontal")
 
-ship_array=np.append(ship_array,ship3)
+# ship_array=np.append(ship_array,ship3)
 
-ship4=Ship(4, np.array([0,9]),"Horizontal")
+# ship4=Ship(4, np.array([0,9]),"Horizontal")
 
-ship_array=np.append(ship_array,ship4)
+#ship_array=np.append(ship_array,ship4)
+ship_array=ship_placement_random()
 
 
 for ship in ship_array:
