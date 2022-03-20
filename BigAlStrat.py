0d000d8dfa014be9b2ca062354a3afac17c61144
amg220 <amg220@ic.ac.uk>
2022-03-20 22:57:37 +0000

    data added

--- BigAlStrat.py
+++ BigAlStrat.py
@@ -40,14 +40,14 @@ def outer(GameState, Ships):
             
     inner(GameState, Ships)
     
-    MaxProb = (0,0)
+    MaxProb = (0,(0,0))
     
     for i in range(n):
         for j in range(m):
             if CountMatrix[i][j] > MaxProb[0]:
-                MaxProb = (i, j)
+                MaxProb = (CountMatrix[i][j], (i, j))
 
-    return MaxProb
+    return MaxProb[1]
 
 def ShipsLeft(Board):
     return 3 - sum(sum(Board==3))/2
@@ -59,10 +59,12 @@ def LocateUnsunk(Board):
                 return (i, j)
 
 def BigAlStrat(Board):
+    n, m = np.shape(Board)
+    
     if sum(sum(Board == 2)) != 0:
         i, j = LocateUnsunk(Board)
         
-        if 0 < i < 8 and 0 < j < 8:  
+        if 0 < i < n-1 and 0 < j < m-1:  
             if Board[i+1][j] == 0 or Board[i+1][j] == 2:
                 return (i+1, j)
             elif Board[i][j+1] == 0 or Board[i][j+1] == 2:
@@ -72,7 +74,7 @@ def BigAlStrat(Board):
             else:
                 return (i, j-1)
             
-        if 0 < i < 8 and j == 0:  
+        if 0 < i < n-1 and j == 0:  
             if Board[i+1][j] == 0 or Board[i+1][j] == 2:
                 return (i+1, j)
             elif Board[i][j+1] == 0 or Board[i][j+1] == 2:
@@ -80,7 +82,7 @@ def BigAlStrat(Board):
             elif Board[i-1][j] == 0 or Board[i-1][j] == 2:
                 return (i-1, j)
             
-        if 0 < i < 8 and j == 8:  
+        if 0 < i < n-1 and j == m-1:  
             if Board[i+1][j] == 0 or Board[i+1][j] == 2:
                 return (i+1, j)
             elif Board[i-1][j] == 0 or Board[i-1][j] == 2:
@@ -88,7 +90,7 @@ def BigAlStrat(Board):
             else:
                 return (i, j-1)
             
-        if i == 8 and 0 < j < 8:  
+        if i == n-1 and 0 < j < m-1:  
             if Board[i][j+1] == 0 or Board[i][j+1] == 2:
                 return (i, j+1)
             elif Board[i-1][j] == 0 or Board[i-1][j] == 2:
@@ -96,7 +98,7 @@ def BigAlStrat(Board):
             else:
                 return (i, j-1)
             
-        if 0 == i and 0 < j < 8:  
+        if 0 == i and 0 < j < m-1:  
             if Board[i+1][j] == 0 or Board[i+1][j] == 2:
                 return (i+1, j)
             elif Board[i][j+1] == 0 or Board[i][j+1] == 2:
@@ -109,19 +111,19 @@ def BigAlStrat(Board):
                 return (i+1, j)
             elif Board[i][j+1] == 0 or Board[i][j+1] == 2:
                 return (i, j+1)
-        if 0 == i and 8 == j:  
+        if 0 == i and m-1 == j:  
             if Board[i+1][j] == 0 or Board[i+1][j] == 2:
                 return (i+1, j)
             else:
                 return (i, j-1)
             
-        if 8 == i and 0 == j:  
+        if n-1 == i and 0 == j:  
             if Board[i][j+1] == 0 or Board[i][j+1] == 2:
                 return (i, j+1)
             elif Board[i-1][j] == 0 or Board[i-1][j] == 2:
                 return (i-1, j)
             
-        if 8 == i and 8 == j:  
+        if n-1 == i and m-1 == j:  
             if Board[i-1][j] == 0 or Board[i-1][j] == 2:
                 return (i-1, j)
             else:
@@ -129,7 +131,105 @@ def BigAlStrat(Board):
             
         
     else:
-        return outer((Board != 0), ShipsLeft(Board))
+        return outer((Board != 0)*1, ShipsLeft(Board))
 
-
-
+data = [30,
+ 24,
+ 23,
+ 31,
+ 14,
+ 13,
+ 27,
+ 26,
+ 23,
+ 26,
+ 16,
+ 25,
+ 14,
+ 30,
+ 26,
+ 31,
+ 31,
+ 26,
+ 22,
+ 13,
+ 32,
+ 29,
+ 35,
+ 26,
+ 30,
+ 30,
+ 16,
+ 19,
+ 31,
+ 23,
+ 29,
+ 23,
+ 18,
+ 31,
+ 33,
+ 23,
+ 17,
+ 19,
+ 29,
+ 27,
+ 29,
+ 27,
+ 19,
+ 25,
+ 30,
+ 27,
+ 35,
+ 30,
+ 18,
+ 30,
+ 35,
+ 27,
+ 26,
+ 24,
+ 23,
+ 23,
+ 25,
+ 29,
+ 33,
+ 23,
+ 18,
+ 23,
+ 23,
+ 22,
+ 30,
+ 29,
+ 26,
+ 29,
+ 23,
+ 24,
+ 19,
+ 29,
+ 14,
+ 23,
+ 29,
+ 28,
+ 29,
+ 14,
+ 23,
+ 29,
+ 32,
+ 13,
+ 23,
+ 25,
+ 19,
+ 25,
+ 19,
+ 30,
+ 34,
+ 30,
+ 26,
+ 28,
+ 14,
+ 23,
+ 17,
+ 19,
+ 24,
+ 30,
+ 34,
+ 29]
